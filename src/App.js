e9f69724e816e30c07cc57fd14dbb47f2b0b935b
dipdastoko <dipdastoko@gmail.com>
2022-08-21 02:16:46 +0600

    Navigation added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
