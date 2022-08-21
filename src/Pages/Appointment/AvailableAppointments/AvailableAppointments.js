442e772dcc3bfbf57edec4563f3f21663fb91b90
dipdastoko <dipdastoko@gmail.com>
2022-08-21 21:29:45 +0600

    booking modal added

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -53,6 +53,7 @@ const AvailableAppointments = ({ date }) => {
                     bookings.map(booking => <Booking
                         key={booking.id}
                         booking={booking}
+                        date={date}
                     />)
                 }
             </Grid>
--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,23 +1,35 @@
 import { Button, Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
+import BookingModal from '../BookingModal/BookingModal';
 
-const Booking = ({ booking }) => {
+const Booking = ({ booking, date }) => {
     const { name, time, space } = booking;
+    const [openBooking, setOpenBooking] = React.useState(false);
+    const handleBookingOpen = () => setOpenBooking(true);
+    const handleBookingClose = () => setOpenBooking(false);
     return (
-        <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{ py: 5 }}>
-                <Typography sx={{ color: 'info.main', fontWeight: 'bold' }} variant='h5'>
-                    {name}
-                </Typography>
-                <Typography variant='h6'>
-                    {time}
-                </Typography>
-                <Typography variant='caption'>
-                    {space} SPACES AVAILABLE
-                </Typography>
-                <Button variant='contained' >BOOK APPOINTMENT</Button>
-            </Paper>
-        </Grid>
+        <>
+            <Grid item xs={12} sm={6} md={4}>
+                <Paper elevation={3} sx={{ py: 5 }}>
+                    <Typography sx={{ color: 'info.main', fontWeight: 'bold' }} variant='h5'>
+                        {name}
+                    </Typography>
+                    <Typography variant='h6'>
+                        {time}
+                    </Typography>
+                    <Typography variant='caption'>
+                        {space} SPACES AVAILABLE
+                    </Typography>
+                    <Button onClick={handleBookingOpen} variant='contained' >BOOK APPOINTMENT</Button>
+                </Paper>
+            </Grid>
+            <BookingModal
+                booking={booking}
+                openBooking={openBooking}
+                handleBookingClose={handleBookingClose}
+                date={date}
+            />
+        </>
     );
 };
 
