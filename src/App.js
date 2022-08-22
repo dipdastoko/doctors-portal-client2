c7d670ea0ef845a94bfaab14e27bbec63c0ceb88
dipdastoko <dipdastoko@gmail.com>
2022-08-22 14:45:37 +0600

    login page created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/home' element={<Home />} />
           <Route path='/appointment' element={<Appointment />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
