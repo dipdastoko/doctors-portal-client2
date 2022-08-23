cbad9a43c560c68a744a39e340571581c88be672
dipdastoko <dipdastoko@gmail.com>
2022-08-23 18:03:17 +0600

    dashboard created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AuthProvider from './Context/AuthProvider/AuthProvider';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
@@ -17,6 +18,9 @@ function App() {
             <Route path='/home' element={<Home />} />
             <Route path='/appointment' element={<PrivateRoute>
               <Appointment />
+            </PrivateRoute>} />
+            <Route path='/dashboard' element={<PrivateRoute>
+              <Dashboard />
             </PrivateRoute>} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
