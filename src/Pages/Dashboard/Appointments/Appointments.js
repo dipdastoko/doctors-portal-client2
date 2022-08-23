bfd6c6a6c9b843023c6e59ed6658fce7d96dde46
dipdastoko <dipdastoko@gmail.com>
2022-08-23 19:14:54 +0600

    Dashboard updated

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -8,16 +8,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const Appointments = () => {
+const Appointments = ({ date }) => {
     const { user } = useAuth();
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const url = `http://localhost:5000/appointments?email=${user.email}`
+        const url = `http://localhost:5000/appointments?email=${user.email}&date=${date}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setAppointments(data))
-    }, [user.email])
+    }, [user.email, date])
     return (
         <TableContainer component={Paper}>
             <Table sx={{}} aria-label="simple table">
@@ -25,6 +25,7 @@ const Appointments = () => {
                     <TableRow>
                         <TableCell>Name</TableCell>
                         <TableCell align="right">Time</TableCell>
+                        <TableCell align="right">Service</TableCell>
                         <TableCell align="right">Action</TableCell>
                     </TableRow>
                 </TableHead>
@@ -38,6 +39,7 @@ const Appointments = () => {
                                 {row.patientName}
                             </TableCell>
                             <TableCell align="right">{row.time}</TableCell>
+                            <TableCell align="right">{row.serviceName}</TableCell>
                             <TableCell align="right">{row.fat}</TableCell>
                         </TableRow>
                     ))}
